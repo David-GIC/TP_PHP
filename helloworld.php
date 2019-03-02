669db4d7c0f9450411fad25bffe4a2fc1566fc4c
David-GIC <41237774+David-GIC@users.noreply.github.com>
2019-03-02 09:46:51 +0700

    Add files via upload

--- helloworld.php
+++ helloworld.php
@@ -1,11 +1,14 @@
-<?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
-?>
-Something is wrong with the XAMPP installation :-(
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
+</head>
+<body>
+	<?php
+	echo "Hello World!";
+	?>
+</body>
+</html>
